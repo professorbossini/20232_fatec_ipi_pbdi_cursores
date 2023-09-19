963c11721bebf91f99d821e351038f161a21341f
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-19 11:09:50 -0300

    ap16(cursores): fim da apostila

--- script.sql
+++ script.sql
@@ -1,27 +1,154 @@
 DO $$
 DECLARE
-	--1. Declaração do cursor
-	--não vinculado
-	cur_nomes_youtubers REFCURSOR;
-	-- para armazenar o nome da vez
-	v_youtuber VARCHAR(200);
+	--1. Declaração do cursor (unbound (não vinculado))
+	cur_delete REFCURSOR;
+	tupla RECORD;
 BEGIN
 	--2. Abertura do cursor
-	OPEN cur_nomes_youtubers FOR
-		SELECT youtuber
-		FROM
-		tb_top_youtubers;
+	--scroll para descer e subir
+	OPEN cur_delete SCROLL FOR
+		SELECT * FROM tb_top_youtubers;
 	LOOP
-		--3. Recuperação dos dados de interesse
-		FETCH cur_nomes_youtubers INTO v_youtuber;
-		--variável especial: FOUND
+		--3. Recuperação de dados
+		FETCH cur_delete INTO tupla;
 		EXIT WHEN NOT FOUND;
-		RAISE NOTICE 'Nome: %', v_youtuber;
+		IF tupla.video_count IS NULL THEN
+			DELETE FROM 
+				tb_top_youtubers 
+			WHERE CURRENT OF cur_delete;
+		END IF;
 	END LOOP;
-	--4. Fechar o cursor
-	CLOSE cur_nomes_youtubers;
+	
+	LOOP
+		FETCH BACKWARD FROM cur_delete INTO tupla;
+		EXIT WHEN NOT FOUND;
+		RAISE NOTICE '%', tupla;
+	END LOOP;
+	
+	--4. Fechamento do cursor
+	CLOSE cur_delete;
+	
 END;
 $$
+
+--cursores com parâmetro
+-- DO $$
+-- DECLARE
+-- 	v_ano INT := 2010;
+-- 	v_inscritos INT := 60000000;
+-- 	v_youtuber VARCHAR(200);
+	
+-- 	-- vinculado (bound)
+-- 	--1. Declaração do cursor
+-- 	cur_ano_inscritos CURSOR (ano INT, inscritos INT)
+-- 	FOR SELECT youtuber FROM tb_top_youtubers WHERE
+-- 	started >= ano AND subscribers >= inscritos;	
+-- BEGIN
+-- 	--passagem pela ordem
+-- 	--OPEN cur_ano_inscritos(v_ano, v_inscritos);
+-- 	--passagem por nome
+-- 	--2. Abertura do cursor
+-- 	OPEN cur_ano_inscritos
+-- 			(inscritos := v_inscritos, ano := v_ano);
+	
+-- 	LOOP
+-- 		--3. Recuperação de dados
+-- 		FETCH cur_ano_inscritos INTO v_youtuber;
+-- 		EXIT WHEN NOT FOUND;
+-- 		RAISE NOTICE '%', v_youtuber;
+-- 	END LOOP;
+-- 	--4. Fechamento do cursor
+-- 	CLOSE cur_ano_inscritos;
+-- END;
+-- $$
+
+
+
+--cursor vinculado (bound)
+--nome do youtuber e o número de inscritos
+--concatenando e exibindo apenas no final
+-- DO $$
+-- DECLARE
+-- 	--1. Declaração (já nasceu vinculado)
+-- 	cur_nomes_e_inscritos CURSOR FOR SELECT youtuber, subscribers FROM tb_top_youtubers;
+-- 	--um record pode representar uma linha
+-- 	--tupla.nome dá acesso ao valor na coluna nome
+-- 	tupla RECORD;
+-- 	resultado TEXT DEFAULT '';
+-- BEGIN
+-- 	--2. Abrir o cursor
+-- 	OPEN cur_nomes_e_inscritos;
+-- 	--3. Recuperação dos dados
+-- 	FETCH cur_nomes_e_inscritos INTO tupla;
+-- 	WHILE FOUND LOOP
+-- 		resultado := resultado || tupla.youtuber || ':' || tupla.subscribers || ',';
+-- 		--3. Recuperação dos dados
+-- 		FETCH cur_nomes_e_inscritos INTO tupla;
+-- 	END LOOP;
+-- 	--4. Fechar o cursor
+-- 	CLOSE cur_nomes_e_inscritos;
+-- 	RAISE NOTICE '%', resultado;
+-- END;
+-- $$
+
+
+--cursor unbound com query dinâmica
+--dinâmica: armazenada como string
+--exibir canais que começaram somente a partir
+--de um ano específico
+-- DO $$
+-- DECLARE
+-- 	--1. Declaração do cursor
+-- 	cur_nomes_a_partir_de REFCURSOR;
+-- 	v_youtuber VARCHAR(200);
+-- 	v_ano INT := 2008;
+-- 	v_nome_tabela VARCHAR(200) := 'tb_top_youtubers';
+-- BEGIN
+-- 	--2. Abertura do cursor
+-- 	OPEN cur_nomes_a_partir_de FOR EXECUTE
+-- 	format
+-- 	(
+-- 	'
+-- 		SELECT youtuber FROM %s	WHERE started >= $1
+-- 	',
+-- 		v_nome_tabela
+-- 	)USING v_ano;
+-- 	LOOP
+-- 		--3. Recuperação dos dados
+-- 		FETCH cur_nomes_a_partir_de INTO v_youtuber;	
+-- 		EXIT WHEN NOT FOUND;
+-- 		RAISE NOTICE '%', v_youtuber;		
+-- 	END LOOP;
+-- 	--4. Fechar o cursor
+-- 	CLOSE cur_nomes_a_partir_de;
+-- END;
+-- $$
+
+
+-- DO $$
+-- DECLARE
+-- 	--1. Declaração do cursor
+-- 	--não vinculado
+-- 	cur_nomes_youtubers REFCURSOR;
+-- 	-- para armazenar o nome da vez
+-- 	v_youtuber VARCHAR(200);
+-- BEGIN
+-- 	--2. Abertura do cursor
+-- 	OPEN cur_nomes_youtubers FOR
+-- 		SELECT youtuber
+-- 		FROM
+-- 		tb_top_youtubers;
+-- 	LOOP
+-- 		--3. Recuperação dos dados de interesse
+-- 		FETCH cur_nomes_youtubers INTO v_youtuber;
+-- 		--variável especial: FOUND
+-- 		EXIT WHEN NOT FOUND;
+-- 		RAISE NOTICE 'Nome: %', v_youtuber;
+-- 	END LOOP;
+-- 	--4. Fechar o cursor
+-- 	CLOSE cur_nomes_youtubers;
+-- END;
+-- $$
 
 
 -- SELECT * FROM tb_top_youtubers;
